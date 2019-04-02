318786bfb4909bd05b24484b0f4d0ff184f6da56
Ankeet Maini <ankeet.maini@gmail.com>
2019-04-02 19:00:09 +0530

    Adds text input and filters restaurants

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 import { HeaderStyle } from "./HeaderStyle";
 const restaurants = [
   {
@@ -16,30 +16,41 @@ const restaurants = [
   }
 ];
 export default class App extends Component {
+  state = {
+    search: ""
+  };
   render() {
     return (
       <View>
         <Text style={HeaderStyle.header}>Restaurant Review</Text>
-        {restaurants.map((restaurant, index) => (
-          <View
-            key={restaurant.name}
-            style={[
-              styles.row,
-              { backgroundColor: index % 2 === 0 ? "transparent" : "#f3f3f7" }
-            ]}
-          >
-            <View style={styles.edges}>
-              <Text>{index + 1}</Text>
+        <TextInput
+          placeholder="Live Search"
+          style={styles.search}
+          value={this.state.search}
+          onChangeText={text => this.setState({ search: text })}
+        />
+        {restaurants
+          .filter(r => r.name.includes(this.state.search))
+          .map((restaurant, index) => (
+            <View
+              key={restaurant.name}
+              style={[
+                styles.row,
+                { backgroundColor: index % 2 === 0 ? "transparent" : "#f3f3f7" }
+              ]}
+            >
+              <View style={styles.edges}>
+                <Text>{index + 1}</Text>
+              </View>
+              <View style={styles.nameAndAddress}>
+                <Text>{restaurant.name}</Text>
+                <Text style={styles.quiet}>{restaurant.address}</Text>
+              </View>
+              <View style={styles.edges}>
+                <Text>Info</Text>
+              </View>
             </View>
-            <View style={styles.nameAndAddress}>
-              <Text>{restaurant.name}</Text>
-              <Text style={styles.quiet}>{restaurant.address}</Text>
-            </View>
-            <View style={styles.edges}>
-              <Text>Info</Text>
-            </View>
-          </View>
-        ))}
+          ))}
       </View>
     );
   }
@@ -61,5 +72,15 @@ const styles = StyleSheet.create({
   },
   quiet: {
     color: "grey"
+  },
+  search: {
+    marginBottom: 30,
+    padding: 10,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    color: "#444",
+    borderBottomWidth: 1,
+    borderColor: "#ddd",
+    backgroundColor: "#f5f5f5"
   }
 });
